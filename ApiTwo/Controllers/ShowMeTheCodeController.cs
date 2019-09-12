c03fdf4a5d7bf851e1ca77f601e580d7cc326d93
Filipe Andrade <filipe.dsandrade@gmail.com>
2019-09-11 22:45:44 -0300

    Criado método que irá retornar a url do Github que contém o código fonte da Api

--- ApiTwo/Controllers/ShowMeTheCodeController.cs
+++ ApiTwo/Controllers/ShowMeTheCodeController.cs
@@ -2,10 +2,21 @@
 
 namespace ApiTwo.Controllers
 {
+    /// <summary>
+    /// Controller responsável pelo método que retorna a url do Github que contém o código fonte
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ShowMeTheCodeController : ControllerBase
     {
-       
+        /// <summary>
+        /// Método responsável por retornar a url do Github que contém o código fonte da Api atual
+        /// </summary>
+        /// <returns>Retorna uma string com a url do Github que contém o código fonte</returns>
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return new string("https://github.com/FilipeAndrade/ApiTwo");
+        }
     }
 }
