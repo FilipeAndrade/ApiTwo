57ee294eff2243a703345910ff83f7b355a9df3e
Filipe Andrade <filipe.dsandrade@gmail.com>
2019-09-11 22:36:28 -0300

    Add project files.

---
+++ ApiTwo/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTwo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+       
+    }
+}
